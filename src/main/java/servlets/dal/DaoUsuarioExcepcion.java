c9936d71c8ebc206ac52342e4965b982258115c4
CarlosIsla99 <isla.carlos.99@gmail.com>
2022-06-17 13:06:28 +0200

    DaoFabrica

---
+++ src/main/java/servlets/dal/DaoUsuarioExcepcion.java
@@ -0,0 +1,43 @@
+package servlets.dal;
+
+import servlets.modelos.Usuario;
+
+class DaoUsuarioExcepcion implements DaoUsuario {
+
+	@Override
+	public Iterable<Usuario> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario obtenerPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertar(Usuario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificar(Usuario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void borrar(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuario obtenerPorEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
